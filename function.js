e0a679ac54700c2d87e97e23db380c5da4be0981
goddash1 <133566104+goddash1@users.noreply.github.com>
2023-05-25 10:17:30 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,52 @@
+window.function = function (body_input) {
+
+// const glideamount = in_amount.value;
+// const gliderecip = String(in_recip.value) ?? "";
+const body = String(body_input.value) ?? "";
+
+const secret = 'nhYFNjbVKLfNGElt5blyDIMQWz4bCfho66uYSE4JakDX58Ri5ZUBW0i9l3L9aTP0A6TD2XhZ2QCsHa8FHr9j0Fog4Id86asUfWHM9mSDQOfSaaBvhogzUScjveqoEJWT7i5Pq1Npy9QT3v4TO4dM6obUzxOCwpoRh9xDbmaLopaenkxjvYQdhNnF0ybJgxxhGbg1Wz1aAVx6kBZRtu9HAxkN5iv35OT75uB8sADGf1juRtnQB2WQuV6cMWfuNSaD';
+// const data = {
+//   currencyID: 3,
+//   amount: glideamount,
+//   recipientTypeID: 1,
+//   recipient: `${glideorder}`,
+//   clientOrderID: `${gliderecip}`,
+//   webhookURL: 'https://webhook.site/6c2fa6fa-ec9f-47ca-a2c2-18505a6dec9b'
+// };
+// const data = {
+//   amount: glideamount,
+//   currencyID: 3,
+//   typeID: 4,
+//   clientOrderID: gliderecip,
+//   TTL: 999,
+//   webhookURL: "https://webhook.site/c7e3b35d-3e0b-42e7-a46b-e51b98e865c4",
+//   extra: {
+//     comment: "2legs",
+//     allowedPaymentTypeIDs: [
+//       3
+//     ],
+//     allowedPaymentSystemIDs: [
+//       1,
+//       2
+//     ],
+//     allowedCardIssuerIDs: [
+//       1,
+//       2,
+//       3,
+//       4,
+//       5,
+//       6,
+//       7,
+//       8
+//     ],
+//     failedRedirectURL: "https://krossberi.glide.page/dl/failure",
+//     successRedirectURL: "https://krossberi.glide.page/dl/success"
+//   }
+// }
+
+//const raw = JSON.stringify(body_input);
+const hash = CryptoJS.HmacSHA512(body, secret).toString();
+
+return hash;
+
+}
